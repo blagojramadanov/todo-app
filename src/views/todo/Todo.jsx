c1f96ff5ddb1ae888f3cc578f6233a719d484555
Blagoj Ramadanov <blaze.ramadanov@gmail.com>
2026-04-14 21:18:42 +0200

    todoItem and ChangeCeckbox

--- src/views/todo/Todo.jsx
+++ src/views/todo/Todo.jsx
@@ -1,34 +1,20 @@
 import { useState } from "react";
 import TodoItem from "./components/todoitem/TodoItem";
-import Button from "../../components/button/Button";
 
 function Todo() {
-  const [todo, setTodo] = useState({
-    id: Math.random(),
-    content: "NAJACE",
-    done: false,
-  });
+  const [todos, setTodos] = useState([
+    { id: Math.random(), content: "NAJACE", done: true },
+  ]);
 
-  function handleChangeCheckbox() {
-    setTodo({
-      ...todo,
-      done: !todo.done,
-    });
-  }
-
-  function handleAddTodo() {
-    setTodo({
-      id: Math.random(),
-      content: "NOV TODO",
-      done: false,
-    });
+  function handleChangeCheckbox(event) {
+    console.log(event.target.checked);
   }
 
   return (
     <div>
-      <TodoItem todoItem={todo} handleChangeCheckbox={handleChangeCheckbox} />
-
-      <Button onClick={handleAddTodo} text="Add" />
+      {todos.map((todo) => (
+        <TodoItem todoItem={todo} handleChangeCheckbox={handleChangeCheckbox} />
+      ))}
     </div>
   );
 }
